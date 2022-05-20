14a57487f57779caceb2b0b44fae340b62296873
dispofuchs <99131497+dispofuchs@users.noreply.github.com>
2022-05-20 03:04:37 +0200

    Update guildMemberAdd.js

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -15,8 +15,8 @@ module.exports = class extends Event {
             const embed = new MessageEmbed().setTitle("Willkommen auf unseren Server!")
                 .setColor("#2F3136")
                 .setThumbnail(member.user.displayAvatarURL({ dynamic: true }))
-                .addField("⤷Calopia⤶", `──────────✧❁✧──────────\nIch hoffe dir gefällt der Aufenthalt auf unserem Server.\nBitte lies dir die <#${this.client.constants.channels.rules}> und gebe dir anschließend <#${this.client.constants.channels.roles}>.\nHier wirst du nicht in Vergessenheit geraten!\n──────────✧❁✧──────────`)
-                .setImage("https://media.discordapp.net/attachments/897206631140569198/939709423360802856/dbb9nym-e61caf35-aaa1-4968-b233-029e35976dbd.gif?width=1025&height=354")
+                .addField("⤷Rumknutschriff⤶", `──────────✧❁✧──────────\nIch hoffe dir gefällt der Aufenthalt auf unserem Server.\nBitte lies dir die <#${this.client.constants.channels.rules}> und gebe dir anschließend <#${this.client.constants.channels.roles}>.\nHier wirst du nicht in Vergessenheit geraten!\n──────────✧❁✧──────────`)
+                .setImage("https://y.yarn.co/76c2e67b-8fbd-4ffd-bbff-245bd292424d_text.gif")
                 this.client.guild.channels.fetch(this.client.constants.channels.generalChat).then(channel => channel.send({content: `<@${member.user.id}>` ,embeds: [embed]}))
         }
     }
